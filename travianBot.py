0b145e85cd6eca59eb6a04acdac117056a13d9bb
Unknown <capkovicdenis@gmail.com>
2018-05-17 16:32:06 +0200

    Class implementation

--- travianBot.py
+++ travianBot.py
@@ -5,16 +5,20 @@ import Build
 starting_url = 'https://ts1.travian.cz/dorf1.php'
 
 
-def login():
-    '''prihlasi sa na ucet a vrati session'''
-    data = {'login': '1526489272', 'name': 'Scasike', 'password': 'firebrand', 's1': 'Přihlásiť+se',
-            'w': '1920:1080'}
+class Travian:
+    def __init__(self, username, password):
+        self.session = self.login(username, password)
 
-    with requests.session() as session:
-        session.headers.update({'x-test': 'true'})
-        session.post(starting_url, data)
-        return session
+    def login(self, username, password):
+        '''prihlasi sa na ucet a vrati session'''
+        data = {'login': '1526489272', 'name': username, 'password': password, 's1': 'Přihlásiť+se',
+                'w': '1920:1080'}
 
+        with requests.session() as session:
+            session.headers.update({'x-test': 'true'})
+            session.post(starting_url, data)
+            return session
 
-session=login()
-Build.repetitive_build(session)
+if __name__ == '__main__':
+    travian = Travian("Scasike", "firebrand")
+    Build.repetitive_build(session)
