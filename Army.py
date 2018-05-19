7a7708e4a7af1e23653e62203c180fddefffbd77
Ancient <jakub.vins2@gmail.com>
2018-05-19 14:44:58 +0200

    Converting Army to class

--- Army.py
+++ Army.py
@@ -1,17 +1,34 @@
 import travianBot
 from bs4 import BeautifulSoup
 
-def get_troops(session,url):
+class Army:
+    def __init__(self,session,url,debug):
+        self.session = session
+        self.debug = debug
+        self.url = url + '/dorf1.php'
+        self.troop_send_url = url + '/build.php?tt=2&id=39'
 
-    r = session.get(url)
-    soup = BeautifulSoup(r.content, 'html.parser')
-    keys = soup.find_all('td',{'class': 'un'})
-    keys = [k.text for k in keys]
-    values = soup.find_all('td',{'class': 'num'})
-    values = [v.text for v in values[4:]]
-    troops=dict(zip(keys,values))
-    print(troops)
+    def get_soup(self,url):
+        return BeautifulSoup(self.session.get(url).content, 'html.parser')
+
+    def get_troops(self):
+        soup = self.get_soup(self.url)
+        keys = soup.find_all('td',{'class': 'un'})
+        keys = [k.text for k in keys]
+        values = soup.find_all('td',{'class': 'num'})
+        values = [v.text for v in values[4:]]
+        troops = dict(zip(keys,values))
+        return troops
+
+    # def sent_troops(self,troops,x,y):
+    #     soup = travian.get_soup(self.troop_send_url)
+    #     for troops.items
+
+    def translator(self,troops):
+        '''premeni keys(f.e.Pálkařů) na t1,t2..'''
+        table = {'Pálkařů': 't1'}
+        translated = {table[k]: v for k, v in troops.items()}
+        return translated
 
 if __name__ == "__main__":
     travian = travianBot.Travian("Scasike", "firebrand",'https://ts3.travian.cz/dorf1.php')
-    get_troops(travian.session,travian.url)
