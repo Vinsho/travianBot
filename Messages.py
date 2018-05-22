763066938b4e2e5df6dbd2db721f124f7a32efa9
Ancient <jakub.vins2@gmail.com>
2018-05-22 14:23:09 +0200

    sick logging system

--- Messages.py
+++ Messages.py
@@ -1,6 +1,21 @@
 import time
 import datetime
+import logging
 
-def build_at(rand_secs):
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
+                    filename='log.txt',
+                    filemode='w')
+
+
+def next_build(rand_secs):
     buildin_in = time.time() + rand_secs
-    print("next build at: " + datetime.datetime.fromtimestamp(buildin_in).strftime('%Y-%m-%d %H:%M:%S'))
+    logging.warning("Next build at: " + datetime.datetime.fromtimestamp(buildin_in).strftime('%Y-%m-%d %H:%M:%S') + '\n')
+
+
+def building(res, id, lvl):
+    logging.warning("Building " + res + " with id=" + str(id + 1) + ' to lvl '+str(lvl+1) + '\n')
+
+
+def write_excep(s,msg):
+    up_in = time.time() + s
+    logging.warning(msg+ ", puttin myself to sleep, will be up at " + datetime.datetime.fromtimestamp(up_in).strftime('%Y-%m-%d %H:%M:%S') + '\n')
